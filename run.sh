d0d7bf95e19855385d2e8b665103aa91fb6e3cbf
alexandergomez2134 <60235266+alexandergomez2134@users.noreply.github.com>
2021-03-21 22:34:11 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,36 @@
+echo "The script file for Sum of an Array has begun"
 
+#Program: Sum of an Array
+#Author: Alexander Gomez
+
+#Delete some un-needed files
+rm *.o
+rm *.out
+rm *.lis
+
+echo "Compile main.c"
+gcc -c -Wall -m64 -no-pie -o main.o main.c -std=c11
+
+echo "Compile control.asm"
+nasm -f elf64 -l control.lis -o control.o control.asm
+
+echo "Compile fill.asm"
+nasm -f elf64 -l fill.lis -o fill.o fill.asm
+
+echo "Compile sum.asm"
+nasm -f elf64 -l sum.lis -o sum.o sum.asm
+
+echo "Compile display.cpp"
+gcc -c -Wall -m64 -no-pie -o display.o display.cpp -std=c++17
+
+echo "Link the object files"
+gcc -m64 -no-pie -o assignment3.out -std=c++17 main.o control.o fill.o sum.o display.o
+
+echo "Run the program assignment3"
+./assignment3.out
+
+echo "Delete some un-needed files"
+rm *.o
+rm *.lis
+
+echo "The script file will terminate"
